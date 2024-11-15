0a22ea136dae8fcf54442f3298c39c7a6a7c76a7
priya <priyankasatdive89@gmail.com>
2024-11-15 15:13:13 +0530

    changes made in python file on locally

--- app1.py
+++ app1.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 
+
 app = Flask(__name__)
 
 # Route to render the calculator HTML page
@@ -37,3 +38,5 @@ def calculate():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+    print("Priyanka satdive")
+    print("\n Prn:240840141017")
